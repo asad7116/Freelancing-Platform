3c7e483a6b42dc4df617032f4a77ea74a4b99ff9
Asad Mahmood <asadmeh.9546@gmail.com>
2025-08-22 09:37:25 +0500

    Add Header

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import "../styles/Header.css";
+
+const Header = () => {
+  return (
+    <header>
+      {/* Top Navbar */}
+      <div className="navbar">
+        {/* Logo */}
+        <div className="logo">
+          <img src="/assets/logo/logo.png" alt="logo" />
+          <h1>
+            <span className="green">Work</span>zone
+          </h1>
+        </div>
+
+        {/* Navigation */}
+        <nav className="nav-links">
+          <a href="#">Home</a>
+          <a href="#">Services</a>
+          <a href="#">Job Post</a>
+          <a href="#">Freelancers</a>
+          <a href="#">Pages ▾</a>
+          <a href="#">Contact</a>
+        </nav>
+
+        {/* Right Side */}
+        <div className="right-side">
+          <span>$ USD</span>
+          <span>🌐 English</span>
+          <button className="signin-btn">Sign In</button>
+        </div>
+      </div>
+
+      {/* Sub Navbar */}
+      <div className="sub-navbar">
+        <a href="#">Marketing</a>
+        <a href="#">Architecture</a>
+        <a href="#">Web Design</a>
+        <a href="#">AI Services</a>
+        <a href="#">Business Style</a>
+        <a href="#">Development & IT</a>
+        <a href="#">Photography</a>
+        <a href="#">Design & Creative</a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
