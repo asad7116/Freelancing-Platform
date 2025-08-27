a96220d52dced8ea5ccfd8ff82123c1b885d26a3
Asad Mahmood <asadmeh.9546@gmail.com>
2025-08-27 17:18:18 +0500

    Add Hero Section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,16 @@
-// src/App.js
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import SignIn from "./pages/Signin";
+import SignUp from "./pages/signUp";
 
-function App() {
+export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signUp" element={<SignUp />} />
       </Routes>
     </Router>
   );
 }
-
-export default App;
