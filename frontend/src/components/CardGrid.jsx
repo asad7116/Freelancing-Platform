0d8eb5ef41674a962c5082a20b36ba709eb055ed
Muhammad Sheraz <m.sheraz.work@gmail.com>
2025-09-07 20:16:31 +0500

    Freelancer's inner Page is Linked and created.

--- frontend/src/components/CardGrid.jsx
+++ frontend/src/components/CardGrid.jsx
@@ -1,59 +1,13 @@
 import React from "react";
 import ProfileCard from "./ProfileCard";
 
-export default function CardGrid() {
-  const freelancers = [
-    {
-      name: "David Richard",
-      role: "Web Developer",
-      rating: 4.0,
-      reviews: 10,
-      image: "/assets/Freelancers/david.png",
-      price: 50,
-      isTopSeller: true,
-    },
-    {
-      name: "David Simmons",
-      role: "Web Developer",
-      rating: 0.0,
-      reviews: 0,
-      image: "/assets/Freelancers/david.png",
-      price: 40,
-      isTopSeller: true,
-    },
-    {
-      name: "Naymr Jhon",
-      role: "Graphic Designer",
-      rating: 0.0,
-      reviews: 0,
-      image: "/assets/Freelancers/david.png",
-      price: 45,
-      isTopSeller: true,
-    },
-    {
-      name: "Madge Jordan",
-      role: "Graphic Designer",
-      rating: 0.0,
-      reviews: 0,
-      image: "/assets/Freelancers/david.png",
-      price: 60,
-      isTopSeller: true,
-    },
-    {
-      name: "David Miller",
-      role: "Web Designer",
-      rating: 3.0,
-      reviews: 1,
-      image: "/assets/Freelancers/david.png",
-      price: 55,
-      isTopSeller: false,
-    },
-  ];
-
+export default function CardGrid({ freelancers }) {
   return (
     <div className="card-grid">
       {freelancers.map((freelancer, index) => (
-        <ProfileCard key={index} {...freelancer} />
+        <a href={`/seller/${freelancer.username}`} key={index} style={{ textDecoration: 'none' }}>
+         <ProfileCard key={index} {...freelancer} />
+        </a>
       ))}
     </div>
   );
