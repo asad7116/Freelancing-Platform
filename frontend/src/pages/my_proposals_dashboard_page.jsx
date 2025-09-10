07810cc4c4cdc22d18a43c2d944a370c942b45c1
SaifRiaz3256 <saifriaz1234@gmail.com>
2025-09-11 01:18:49 +0500

    My proposals compoent add

---
+++ frontend/src/pages/my_proposals_dashboard_page.jsx
@@ -0,0 +1,18 @@
+// src/pages/orders_page.jsx
+import React from "react";
+
+import Footer from "../components/Footer";
+import MyProposals from "../components/my_proposals_dashboard";
+
+export default function OrdersPage() {
+  return (
+    <>
+      <MyProposals />
+      <div className="dz-offset">
+          <Footer />
+      </div>
+
+      
+    </>
+  );
+}
